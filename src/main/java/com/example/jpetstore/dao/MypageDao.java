986c9996d49051da4aa96b35db7d45281bbef8ce
dev_me <gjalsdud1030@naver.com>
2024-04-13 16:29:06 +0900

    [FIX] 닉네임 변경 DAO 코드 수정 #3

--- src/main/java/com/example/jpetstore/dao/MypageDao.java
+++ src/main/java/com/example/jpetstore/dao/MypageDao.java
@@ -7,9 +7,6 @@ import org.springframework.dao.DataAccessException;
 
 public interface MypageDao {
 
-    //솔직히 이 아이는 왜 있어야하는지는 모르겠습니다...
-    //정리 좀 하고 나중에 교수님께 여쭤보려고 합니다..
-
     public User getUserById(Long userId) throws DataAccessException;
     //닉네임 변경
     public int updateNickname(String userId, String newNickname) throws DataAccessException;
