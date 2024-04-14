b01ba9ec1e2ae6ed3dc3f3bfad8f83b2c09fd74a
Dayun Lee <wohd7877@naver.com>
2024-04-14 17:45:17 +0900

    [feat/#9]MybatisUser 수정

--- src/main/java/com/example/jpetstore/dao/mybatis/MybatisUserDao.java
+++ src/main/java/com/example/jpetstore/dao/mybatis/MybatisUserDao.java
@@ -1,14 +1,18 @@
 package com.example.jpetstore.dao.mybatis;
 
 import com.example.jpetstore.dao.UserDao;
+import com.example.jpetstore.dao.mybatis.mapper.MyPageMapper;
+import com.example.jpetstore.dao.mybatis.mapper.UserMapper;
 import com.example.jpetstore.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MybatisUserDao implements UserDao {
 
-
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public User getUserById(Long userId) throws DataAccessException {
         return null;
