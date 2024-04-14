905780cb94bf8c3eef98ab4f1751c7f8193a908e
yjin-jo <dpwls5100@icloud.com>
2024-04-14 16:53:55 +0900

    [add] : Review 도메인 구현 #7

--- src/main/java/com/example/jpetstore/domain/Review.java
+++ src/main/java/com/example/jpetstore/domain/Review.java
@@ -1,2 +1,15 @@
-package com.example.jpetstore.domain;public class Review {
+package com.example.jpetstore.domain;
+
+public class Review {
+    String content;
+    int star;
+    int mvId;
+    int id;
+
+    public Review(String content, int star, int mvId, int id) {
+        this.content = content;
+        this.star = star;
+        this.mvId = mvId;
+        this.id = id;
+    }
 }
