4d2a4d0bd56b9b01eb321cc6e783205078ab74d0
yjin-jo <dpwls5100@icloud.com>
2024-04-14 16:54:20 +0900

    [add] : ReviewImpl 구현 #7

--- src/main/java/com/example/jpetstore/service/ReviewImpl.java
+++ src/main/java/com/example/jpetstore/service/ReviewImpl.java
@@ -1,2 +1,18 @@
-package com.example.jpetstore.service;public class ReviewImpl {
+package com.example.jpetstore.service;
+
+import com.example.jpetstore.dao.mybatis.MybatisReviewDao;
+import com.example.jpetstore.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewImpl {
+    private final MybatisReviewDao mybatisReviewDao;
+
+    public ReviewImpl(MybatisReviewDao mybatisReviewDao) {
+        this.mybatisReviewDao = mybatisReviewDao;
+    }
+
+    public void insertMyReview(Review review) {
+        mybatisReviewDao.insertMyReview(review);
+    }
 }
