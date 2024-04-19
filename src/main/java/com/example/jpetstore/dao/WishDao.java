3952a91f2467a513b893a1b0bcc2fb23f9c20008
Kim mihyeon <somflower621@gmail.com>
2024-04-19 17:52:59 +0900

    [add] 찜한 movie 불러옴 #10

--- src/main/java/com/example/jpetstore/dao/WishDao.java
+++ src/main/java/com/example/jpetstore/dao/WishDao.java
@@ -8,6 +8,6 @@ import java.util.Map;
 
 public interface WishDao {
     List<Movie> getWishMovie(int id); // 찜한 영화 가져오기
-    Review getWishReview(String userId); // 찜한 리뷰 가져오기
+    //Review getWishReview(String userId); // 찜한 리뷰 가져오기
     // public String getMyReview(String userId); // 내가 쓴 리뷰 가져오기
 }
