c3fd0999ff5a3f981e1c61562cfd1d0d7cc18be7
Dayun Lee <wohd7877@naver.com>
2024-05-03 12:54:18 +0900

    [feat/#19] 닉네임 수정 UserService 추가

--- src/main/java/com/example/jpetstore/service/UserService.java
+++ src/main/java/com/example/jpetstore/service/UserService.java
@@ -2,5 +2,5 @@ package com.example.jpetstore.service;
 
 public interface UserService {
     public void updatePassword(Long id, String newPassword);
-
+    public void updateNickname(Long id, String newNickname);
 }
