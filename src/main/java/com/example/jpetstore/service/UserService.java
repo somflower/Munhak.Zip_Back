b439298e79751258a5b638b0337a002f2d27db03
Dayun Lee <wohd7877@naver.com>
2024-05-04 12:18:12 +0900

    [feat/#21] 사용자 삭제 파일 추가

--- src/main/java/com/example/jpetstore/service/UserService.java
+++ src/main/java/com/example/jpetstore/service/UserService.java
@@ -3,4 +3,6 @@ package com.example.jpetstore.service;
 public interface UserService {
     public void updatePassword(Long id, String newPassword);
     public void updateNickname(Long id, String newNickname);
+
+    public void deleteUser(Long id);
 }
