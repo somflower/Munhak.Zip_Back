fd531d52c833a12990874d6c2ed643bbca3dcb08
Dayun Lee <wohd7877@naver.com>
2024-05-09 16:20:53 +0900

    [feat/#26] 사용자 insert UserTest 추가

--- src/test/java/com/example/jpetstore/test/UserTest.java
+++ src/test/java/com/example/jpetstore/test/UserTest.java
@@ -3,14 +3,19 @@ package com.example.jpetstore.test;
 
 import com.example.jpetstore.dao.UserDao;
 import com.example.jpetstore.dao.mybatis.MybatisUserDao;
+import com.example.jpetstore.domain.Interest;
 import com.example.jpetstore.domain.Movie;
 import com.example.jpetstore.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static junit.framework.TestCase.assertNotNull;
@@ -81,8 +86,51 @@ public class UserTest {
             System.out.println("NICKNAME: " + user7.getNickname());
             System.out.println("PASSWORD: " + user7.getPassword());
             System.out.println("HINT: " + user7.getHint());
+
             System.out.println("--------------------------");
         }
+
+        Long userId = 3L;
+//        List<String> genre = new ArrayList<>(); // 예시 관심사
+//        genre.add("0");
+//        genre.add("1");
+//        System.out.println(genre);
+
+        // 사용자의 관심사 추가
+//        Interest interest = new Interest();
+//        interest.setId(userId);
+//        interest.setGenre(genre.toString());
+//        userDao.addInterest(interest);
+
+        //사용자의 관심사 찾기
+        Interest interests = userDao.findInterest(3L);
+        System.out.println("관심사:"+ interests.getGenre());
+
+        //관심사 수정
+//        List<String> genre = new ArrayList<>(); // 예시 관심사
+//        genre.add("1");
+//        genre.add("2");
+//        System.out.println(genre);
+//        userDao.updateInterest(3L, genre.toString());
+
+        //사용자 추가
+        User user9 = new User();
+        user9.setUser_id("안녕");
+        user9.setPassword("hello");
+        user9.setHint("하이");
+        user9.setNickname("hi");
+        userDao.addUser(user9);
+
+        List<String> genre = new ArrayList<>(); // 예시 관심사
+        genre.add("2");
+        genre.add("4");
+        System.out.println(genre);
+
+        Interest interest = new Interest();
+        interest.setId(user9.getId());
+        interest.setGenre(genre.toString());
+        userDao.addInterest(interest);
+
     }
 
 }
