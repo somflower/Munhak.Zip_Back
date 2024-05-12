e8cf02eabffec9e3dc74f7db9344c36fe8b6434f
somflower <somflower621@gmail.com>
2024-05-12 23:31:56 +0900

    [add] wishDao

--- src/main/java/com/example/jpetstore/dao/mybatis/mapper/LoginMapper.java
+++ src/main/java/com/example/jpetstore/dao/mybatis/mapper/LoginMapper.java
@@ -3,13 +3,8 @@ package com.example.jpetstore.dao.mybatis.mapper;
 import com.example.jpetstore.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.transaction.annotation.Transactional;
 
 @Mapper
 public interface LoginMapper {
-    //DB 접근
-    @Transactional
-    @Select("SELECT * FROM USER_TABLE WHERE USER_ID = #{userId} and PASSWORD = #{password}")
-    public User userLogin(@Param("userId") String userId, @Param("password") String password);
+    User userLogin(@Param("userId") String userId, @Param("password") String password);
 }
